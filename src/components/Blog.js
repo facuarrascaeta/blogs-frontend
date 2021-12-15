41df384c6f0489f2b854f36e3f6869ddfd10d3a9
Facundo Arrascaeta <arrascaeta.facu@gmail.com>
2021-12-15 18:03:08 -0300

    Ejercicio 5.7 paso 7 (5.6 ya estaba hecho)

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,24 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 6,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      <div style={{ display: visible ? '' : 'none'}}>
+        <div>{blog.url}</div>
+        <div>likes: {blog.likes}<button>like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
